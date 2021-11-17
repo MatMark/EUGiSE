06f02c76604ef78fb3631108622737fc988db707
MatMark <mateusz@hexadronic.com>
2021-11-17 23:19:39 +0100

    PKK

--- systemy_ekspertowe/main.py
+++ systemy_ekspertowe/main.py
@@ -5,6 +5,7 @@ class Menu():
     def __init__(self):
         self.last_questions = []
         self.current_question = "categories"
+        self.answers = []
 
     def user_input(self, question, max):
         while(True):
@@ -14,21 +15,21 @@ class Menu():
                 if(p <= max and p >= 0):
                     break
                 else:
-                    print("Wartość musi być liczbą całkowitą ≥ 0 i ≤ {}".format(max-1))
+                    print("Wartość musi być liczbą całkowitą ≥ 0 i ≤ {}".format(max))
             except ValueError:
-                print("Wartość musi być liczbą całkowitą ≥ 0 i ≤ {}".format(max-1))
+                print("Wartość musi być liczbą całkowitą ≥ 0 i ≤ {}".format(max))
         return p
-    
-    def show_answers(self, question: str, answer: str):
+
+    def fill_answers(self, question: str, answer: str):
         self.current_question = question
-        f = open('questions/{}.json'.format(question))
+        f = open('questions/{}.json'.format(question.replace('!', '')))
         q = json.load(f)
         if(q['answers'][answer]):
-            print(q['answers'][answer])
+            self.answers.append(q['answers'][answer])
 
     def show_question(self, question: str):
         self.current_question = question
-        f = open('questions/{}.json'.format(question))
+        f = open('questions/{}.json'.format(question.replace('!', '')))
         q = json.load(f)
         num = 0
         if(len(self.last_questions) > 0):
@@ -39,13 +40,13 @@ class Menu():
             num += 1
             print('{}: {}'.format(num, v))
         val = self.user_input(q['question'], len(q['options']))
-        if(val == 0):
+        if(val == 0 or list(q['options'])[val - 1] == 'back'):
             if(len(self.last_questions) > 0):
                 question = self.last_questions.pop()
                 self.show_question(question)
             else:
                 return
-        elif(list(q['options'])[val - 1] != 'end' and list(q['options'])[val - 1][0:3] != 'wip'):
+        elif(list(q['options'])[val - 1] != 'break' and list(q['options'])[val - 1][0:3] != 'wip'):
             self.last_questions.append(question)
             self.show_question(list(q['options'])[val - 1])
         else:
@@ -53,8 +54,9 @@ class Menu():
             print("\n\n________________________________________")
             for i, a in enumerate(self.last_questions):
                 if(i + 1 < len(self.last_questions)):
-                    self.show_answers(a, self.last_questions[i+1])
-
+                    self.fill_answers(a, self.last_questions[i+1])
+            self.answers = list(dict.fromkeys(self.answers))
+            print('\n'.join(map(str, self.answers)))
 
 
 app = Menu()
